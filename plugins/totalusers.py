d95f200d511ad069372059f84d1fe947b66b1fb9
akabhai <artevern@gmail.com>
2026-03-18 18:02:38 +0530

    Update totalusers.py

--- plugins/totalusers.py
+++ plugins/totalusers.py
@@ -1,39 +1,40 @@
 from config import *
 from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup)
-from helper.database import botdata, find_one, total_user,getid
-from helper.utils import humanbytes
+from helper.database import botdata, find_one, total_user, getid
 
 token = BOT_TOKEN
 botid = token.split(':')[0]
 
-
-
+# Added locally to prevent Circular Import Crash
+def humanbytes(size):
+    if not size: return "0 B"
+    power = 2 ** 10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 @Client.on_message(filters.private & filters.user(ADMIN)  & filters.command(["users"]))
 async def users(client,message):
     botdata(int(botid))
     data = find_one(int(botid))
-    total_rename = data["total_rename"]
-    total_size = data["total_size"]
-    id = str(getid())
-    ids = id.split(',')
+    total_rename = data.get("total_rename", 0)
+    total_size = data.get("total_size", 0)
     
     await message.reply_text(f"<b>⚡️ Total User :</b> {total_user()}\n\n<b>⚡️ Total Renamed File :</b> {total_rename}\n<b>⚡ Total Size Renamed :</b> {humanbytes(int(total_size))}", quote=True, reply_markup= InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Close 🦋", callback_data="cancel")]])
-        )
-	
-    
+        [InlineKeyboardButton("🦋 Close 🦋", callback_data="cancel")]]))
     
 @Client.on_message(filters.private & filters.user(ADMIN)  & filters.command(["allids"]))
 async def allids(client,message):
     botdata(int(botid))
     data = find_one(int(botid))
-    total_rename = data["total_rename"]
-    total_size = data["total_size"]
+    total_rename = data.get("total_rename", 0)
+    total_size = data.get("total_size", 0)
     id = str(getid())
     ids = id.split(',')
     
     await message.reply_text(f"<b>⚡️ All IDs :</b> {ids}\n\n<b>⚡️ Total User :</b> {total_user()}\n\n<b>⚡️ Total Renamed File :</b> {total_rename}\n<b>⚡ Total Size Renamed :</b> {humanbytes(int(total_size))}", quote=True, reply_markup= InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Close 🦋", callback_data="cancel")]])
-        )
+        [InlineKeyboardButton("🦋 Close 🦋", callback_data="cancel")]]))
